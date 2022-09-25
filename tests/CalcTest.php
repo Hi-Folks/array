574dc2ffc42f3d5bd4b0e55ce1d28b143c90455d
Roberto Butti <roberto.butti@gmail.com>
2022-09-25 10:22:20 +0200

    fix assertEquals precision

--- tests/CalcTest.php
+++ tests/CalcTest.php
@@ -3,25 +3,24 @@
 use HiFolks\DataType\Arr;
 
 it('sum Array', function () {
-    $arr = Arr::make([1 ,2 ,3 ]);
+    $arr = Arr::make([1, 2, 3]);
     expect($arr->sum())->toEqual(6);
 
-    $arr = Arr::make(["3" ,2 ,3 ]);
+    $arr = Arr::make(['3', 2, 3]);
     expect($arr->sum())->toEqual(8);
     $arr = Arr::fromValue(1, 100);
     expect($arr->sum())->toEqual(100);
 });
 
 it('avg Array', function () {
-    $arr = Arr::make([1 ,2 ,3 ]);
+    $arr = Arr::make([1, 2, 3]);
     expect($arr->avg())->toEqual(2);
-
 
     $arr = Arr::fromValue(1, 100);
     expect($arr->avg())->toEqual(1);
 
     $arr = Arr::fromValue(1.2, 100);
-    expect($arr->avg())->toEqual(1.2);
+    expect(round($arr->avg(), 2))->toEqual(1.2);
 
     $arr = Arr::make([1.1, 1.5]);
     expect($arr->avg())->toEqual(1.3);
