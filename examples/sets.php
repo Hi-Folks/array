d145fa9761289af03cfa186ac1087779a0eab069
Roberto Butti <roberto.butti@gmail.com>
2022-11-25 08:57:24 +0100

    Create sets.php

---
+++ examples/sets.php
@@ -0,0 +1,14 @@
+<?php
+
+require './vendor/autoload.php';
+
+use HiFolks\DataType\Arr;
+
+// https://medium.com/@alvaro.saburido/set-theory-for-arrays-in-es6-eb2f20a61848
+
+$arrA = Arr::make([1, 3, 4, 5]);
+$arrB = Arr::make([1, 2, 5, 6, 7]);
+
+echo " --- Intersection" . PHP_EOL;
+$intersection = $arrA->filter(fn ($x) => $arrB->includes($x));
+echo $intersection->toString() . PHP_EOL;
